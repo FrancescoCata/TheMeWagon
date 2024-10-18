564a4afeac5fa3bffc94e72ddf892432eb2e514b
Francesco Cataletto <francesco.cataletto@beije.it>
2024-10-18 11:25:18 +0200

    Added useMemo hook in Menu section to improve performance

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -1,45 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import MenuInfoCard from "../components/menuInfoCard/MenuInfoCard";
-// import { FaCoffee, FaHamburger, FaUtensils } from "react-icons/fa";
 import { getRestaurantMenu } from "../services/MenuService";
 
 export default function Menu() {
   const [selectedMenu, setSelectedMenu] = useState("Pizza");
-  const [dishes, setDishes] = useState([])
+  const [dishes, setDishes] = useState([]);
 
-  useEffect(() =>{
-    getDishes()
-  },[selectedMenu])
+  useEffect(() => {
+    getDishes();
+  }, [selectedMenu]);
 
-  const getDishes = async () =>{
-    const res = await getRestaurantMenu(selectedMenu)
-    setDishes(res)
-  }
+  const getDishes = async () => {
+    const res = await getRestaurantMenu(selectedMenu);
+    setDishes(res);
+  };
 
-  useEffect(() =>{
-    renderDishes()
-  },[dishes])
-
-  const renderDishes = () => {
-    if(dishes.length > 0){
+  const memoizedDishes = useMemo(() => {
+    if (dishes.length > 0) {
       return dishes.map((item, key) => (
-        <MenuInfoCard
-          key={key}
-          dishName={item.name}
-          price={item.price}
-        />
+        <MenuInfoCard key={key} dishName={item.name} price={item.price} />
       ));
     }
-  };
+    return null;
+  }, [dishes]);
 
   const handleMenuClick = (menu) => {
     setSelectedMenu(menu);
   };
 
   const menuItems = [
-    { label: "Pizza", icon: 'fa fa-coffee' },
-    { label: "Dryck", icon: 'fa fa-coffee' },
-    { label: "Tillbehör", icon: 'fa fa-coffee' },
+    { label: "Pizza", icon: "fa fa-coffee" },
+    { label: "Dryck", icon: "fa fa-coffee" },
+    { label: "Tillbehör", icon: "fa fa-coffee" },
   ];
 
   return (
@@ -48,7 +40,9 @@ export default function Menu() {
       <div className="text-center mb-8">
         <div className="flex items-center justify-center mb-4">
           <div className="w-12 h-[2px] bg-[#feaf39]"></div>
-          <p className="text-[#feaf39] font-medium px-2 font-pacifico text-xl">Food Menu</p>
+          <p className="text-[#feaf39] font-medium px-2 font-pacifico text-xl">
+            Food Menu
+          </p>
           <div className="w-12 h-[2px] bg-[#feaf39]"></div>
         </div>
         <h2 className="text-[#0f172b] text-3xl font-semibold">
@@ -77,8 +71,9 @@ export default function Menu() {
         })}
       </div>
 
+      {/* Dishes Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
-        {renderDishes()}
+        {memoizedDishes}
       </div>
     </div>
   );
